f164255b8b7c06ea5ae366bb9ccd6e7cc636e6fb
vivi <3467085831@qq.com>
2021-08-29 16:01:48 +0800

    样式处理

--- src/main.js
+++ src/main.js
@@ -8,7 +8,9 @@ import store from './store'
 // 引入 Element
 import ElementUI from 'element-ui'
 // 引入Element 的主题文件
-import 'element-ui/lib/theme-chalk/index.css'
+// import 'element-ui/lib/theme-chalk/index.css'
+// 引入自定义的全局样式文件
+import './styles/index.scss'
 
 // 将Element注册为Vue插件
 Vue.use(ElementUI)
