ebae573376964bcbe2bb30f6784b54bcf0aa1eb1
vivi <3467085831@qq.com>
2021-08-29 10:54:08 +0800

    adjust original catalog

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,12 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <!-- 实例内容 -->
+    <h1>拉钩教育</h1>
+    <!-- 根路由出口，用于渲染路由组件 -->
+    <route-view></route-view>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style lang="scss" scoped>
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
