553c6fe5c8c48318048ede2fb263e153f18645a0
vivi <3467085831@qq.com>
2021-09-02 22:08:06 +0800

    login页面布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,62 @@
 <template>
-  <div class="login">登录</div>
+  <div class="login">
+    <!-- form组件 -->
+    <el-form
+      label-position="top"
+      ref="form"
+      :model="form"
+      label-width="80px"
+    >
+      <el-form-item label="手机号">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input type="password" v-model="form.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">提交</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Login'
+  name: 'LoginIndex',
+  data () {
+    return {
+      form: {
+        name: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    }
+  }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .login {
+    // 容器充满高度
+    height: 100vh;
+    display: flex;
+    // 内容水平垂直居中
+    align-items: center;
+    justify-content: center;
+
+    .el-form {
+      background-color: #fff;
+      width: 300px;
+      padding: 20px;
+      border-radius: 10px;
+
+      .el-button {
+        width: 100%;
+      }
+    }
+  }
+</style>
