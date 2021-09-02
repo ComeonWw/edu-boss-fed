a8a3cea97fea4e9b1ade23a7c307b36fe448182c
vivi <3467085831@qq.com>
2021-09-02 23:26:18 +0800

    表单验证

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,12 +5,13 @@
       label-position="top"
       ref="form"
       :model="form"
+      :rules="rules"
       label-width="80px"
     >
-      <el-form-item label="手机号">
-        <el-input v-model="form.name"></el-input>
+      <el-form-item label="手机号" prop="phone">
+        <el-input v-model="form.phone"></el-input>
       </el-form-item>
-      <el-form-item label="密码">
+      <el-form-item label="密码" prop="password">
         <el-input type="password" v-model="form.password"></el-input>
       </el-form-item>
       <el-form-item>
@@ -26,14 +27,42 @@ export default {
   data () {
     return {
       form: {
-        name: '',
-        password: ''
+        phone: '1902493',
+        password: '1111'
+      },
+      rules: {
+        phone: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { pattern: /^1\d{10}$/, message: '请输入正确的手机号', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 18, message: '密码长度为6-18个字符', trigger: 'blur' }
+        ]
       }
     }
   },
   methods: {
-    onSubmit () {
-      console.log('submit!')
+    // onSubmit () {
+    //   // console.log('submit!')
+    //   this.$refs.form.validate((valid) => {
+    //     if (valid) {
+    //       alert('submit!')
+    //     } else {
+    //       console.log('error submit!')
+    //       return false
+    //     }
+    //   })
+    // }
+    async onSubmit () {
+      try {
+        // 1.表单验证
+        await this.$refs.form.validate()
+        // 2. 请求
+        // 3.响应处理
+      } catch (err) {
+        console.log('验证失败', err)
+      }
     }
   }
 }
