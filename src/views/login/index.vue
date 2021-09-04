ed08321f6b1c8fc872ac478e4bef81bead519a03
vivi <3467085831@qq.com>
2021-09-04 12:58:09 +0800

    登录请求

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -22,13 +22,16 @@
 </template>
 
 <script>
+import request from '@/utils/request'
+import qs from 'qs'
+
 export default {
   name: 'LoginIndex',
   data () {
     return {
       form: {
-        phone: '1902493',
-        password: '1111'
+        phone: '18201288771',
+        password: '111111'
       },
       rules: {
         phone: [
@@ -54,12 +57,28 @@ export default {
     //     }
     //   })
     // }
+
+    // async和await处理一步请求操作
     async onSubmit () {
+      // 使用try catch处理验证结果
       try {
         // 1.表单验证
         await this.$refs.form.validate()
         // 2. 请求
+        const { data } = await request({
+          method: 'POST',
+          headers: { 'content-type': 'application/x-www-form-urlencoded' },
+          url: '/front/user/login',
+          data: qs.stringify(this.form)
+        })
         // 3.响应处理
+        if (data.state !== 1) {
+          return alert(data.message)
+        }
+        // 成功时跳转到首页，使用this.$router.push()
+        this.$router.push({
+          name: 'home'
+        })
       } catch (err) {
         console.log('验证失败', err)
       }
