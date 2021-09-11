b85a9670302c166ab7f2e937d03f71aa38024b39
vivi <3467085831@qq.com>
2021-09-11 20:22:16 +0800

    Axios响应拦截器操作以及错误处理+element-ui局部引入Message

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,8 @@
 // 引入axios
 import axios from 'axios'
 import store from '@/store'
+// 通过局部引入方式引入Element的Message组件功能
+import { Message } from 'element-ui'
 
 // 创建一个axios实例，用于自定义配置axios对象
 const request = axios.create({
@@ -17,6 +19,7 @@ function getBaseURL (url) {
   }
 }
 
+// 请求拦截器
 request.interceptors.request.use(function (config) {
   // 根据请求的url判断基地址，设置给config.baseURL
   config.baseURL = getBaseURL(config.url)
@@ -31,4 +34,39 @@ request.interceptors.request.use(function (config) {
   return config
 })
 
+// 响应拦截器
+request.interceptors.response.use(function (response) {
+  // 状态码为 2xx 都会进⼊这⾥
+  console.log('请求响应成功了', response)
+  return response
+}, function (error) {
+  // 超出 2xx 都会进⼊这⾥
+  if (error.response) {
+    // 请求发送成功，也收到了响应，到状态码超过了2xx(常见错误处理位置)
+    // 1.保存状态码
+    const { status } = error.response
+    let errorMessage = ''
+    // 判断
+    if (status === 400) {
+      errorMessage = '请求参数错误'
+    } else if (status === 401) {
+      // token无效（过期）处理
+    } else if (status === 403) {
+      errorMessage = '没有权限，请联系管理员'
+    } else if (status === 404) {
+      errorMessage = '请求资源不存在'
+    } else if (status >= 500) {
+      errorMessage = '服务端错误，请联系管理员'
+    }
+    Message.error(errorMessage)
+  } else if (error.request) {
+    // 请求发送成功，但没有收到响应
+    Message.error('请求超时，请重试')
+  } else {
+    // 在设置请求时发生了一些失去，触发了错误（未知型错误）
+    Message.error(`请求失败${error.message}`)
+  }
+  // 将失败的错误对象继续抛出，传递给接收响应的处理函数
+  return Promise.reject(error)
+})
 export default request
