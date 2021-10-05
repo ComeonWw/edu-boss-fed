20095d2d83a5f524f2cb98030bdff8770edbd9ec
vivi <3467085831@qq.com>
2021-10-05 12:03:57 +0800

    富文本编辑器图片上传处理

--- src/components/TextEditor/index.vue
+++ src/components/TextEditor/index.vue
@@ -5,11 +5,14 @@
 <script>
 // 引入富文本编辑器
 import E from 'wangeditor'
+import { uploadCourseImage } from '@/services/course'
+
 export default {
   name: 'TextEditor',
   props: {
     value: {
-      type: String
+      type: String,
+      default: ''
     }
   },
   // 由于需要进行DOM 操作,使用mounted钩子
@@ -20,9 +23,19 @@ export default {
   methods: {
     initEditor () {
       const editor = new E(this.$refs.editor)
-      // 设置回调
-      editor.config.onchange = (value) => {
+      // 创建富文本编辑器之前，进行事件设置
+      editor.config.onchange = value => {
         this.$emit('input', value)
+      }
+      // 配置自定义的图片上传功能
+      editor.config.customUploadImg = async (resultFiles, insertImgFn) => {
+        const fd = new FormData()
+        fd.append('file', resultFiles[0])
+        const { data } = await uploadCourseImage(fd)
+        if (data.code === '000000') {
+          // 通过 inserImgFn 传回给富文本编辑器进行显示
+          insertImgFn(data.data.name)
+        }
       }
       editor.create()
       editor.txt.html(this.value)
